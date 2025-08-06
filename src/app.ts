d1f556a2088a4b1ad97da6a08f0f7d955438c5f3
bernardo <bernardo@socarrao.com.br>
2025-08-06 10:07:45 -0300

    init prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
 import fastify from "fastify"
+import { PrismaClient } from '@prisma/client'
+
 
 export const app = fastify()
+
+// Criando conexão com Banco de dados.
+// Para isso, basta instanciar o PrismaClient
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Name',
+        email: 'email'
+    }
+})
